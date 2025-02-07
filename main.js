89530340b11188c59fcc62dae761221b10b5443b
Jerrod Bolton <jerrodbolton2000@gmail.com>
2025-02-07 03:26:44 -0700

    adding values to the JS
    I put the vales of the input in the JS file and also made it lower case so that the statement can be true and return winner.

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+// I need to add the player input into a variable into the file.
+let askTheQuestion = "You won! Can we do a best out of three?";
+// This is a function is going to run when the shot button is hunt.
+// Also this function is going to return a lower case input to check in the If statement
+function gamePlay() {
+  // This is getting that what the play said and putting it inside a variable
+  let theShot = document.getElementById("thePlayerShot").value;
+  // In this line I make play inner to me a lower case by ues a toLowerCase() down below.
+  //  console.log(typeof theShot);
+  //  this is printing in the clg the Lower Case Word
+  //   console.log(theShot === "rock");
+  let lower = theShot.toLowerCase();
+  //   console.log(lower);
+  //   console.log(lower === "rock");
+  if (lower === "rock"){
+    console.log("They put Rock");  
+    // Print the words that I have lost best two out of three.
+    document.getElementById("printMyShot").innerHTML = "Winner";
+    // This is going to print b/c you want to do three rounds
+    document.getElementById("asksTheQuestion").innerHTML = askTheQuestion;
+  }
 
-
-// this is a function that change the h1 in the HTML 
-function love(theWayIFeel){
-// this is the line that going to change the heading In the HTML To a NEw line 
-    return document.getElementById("heading").innerHTML =  "You are  in love with  this website";
-
-
-// if(theWayIFeel === "I Love Her"){
-    
-//  }
+  // Now that I have made this lower Case I can put that in a If statement
 }
