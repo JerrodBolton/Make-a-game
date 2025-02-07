28144ca731c72bbb3557ad8323a40924558c74d9
Jerrod Bolton <jerrodbolton2000@gmail.com>
2025-02-06 23:28:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function(){
 
-    return null;
+
+// this is a function that change the h1 in the HTML 
+function love(theWayIFeel){
+// this is the line that going to change the heading In the HTML To a NEw line 
+    return document.getElementById("heading").innerHTML =  "You are  in love with  this website";
+
+
+// if(theWayIFeel === "I Love Her"){
+    
+//  }
 }
